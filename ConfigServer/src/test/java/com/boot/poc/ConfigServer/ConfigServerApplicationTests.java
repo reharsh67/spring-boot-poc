320f4b44bbe9b4068e35b1b0602771210e4f4e9f
reharsh67 <reharshdeshpande@gmail.com>
2024-11-01 22:43:18 +0530

    initial commit

---
+++ ConfigServer/src/test/java/com/boot/poc/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.poc.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
