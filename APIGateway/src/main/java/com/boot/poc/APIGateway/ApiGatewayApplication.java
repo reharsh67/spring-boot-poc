8e9189e8616d7efdfdd926b3e62fa6515d843179
reharsh67 <reharshdeshpande@gmail.com>
2024-11-09 20:08:23 +0530

    API gateway

--- APIGateway/src/main/java/com/boot/poc/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/com/boot/poc/APIGateway/ApiGatewayApplication.java
@@ -2,6 +2,7 @@ package com.boot.poc.APIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
